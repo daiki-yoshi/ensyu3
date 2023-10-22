5705811e6a4a8ca7822548146aa5d718863d973b
Daiki <chda21075@g.nihon-u.ac.jp>
2023-10-22 21:48:57 +0900

    App.jsxの追加

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/main";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
